9a07aa349421f91e969e9060d2d655c61939cf8a
S.Otzoy <otzoy97@hotmail.com>
2020-09-29 20:35:22 -0600

    ventana principal agregada

---
+++ src/main/java/com/usac/ipc1/p2/PlotterFrame.java
@@ -0,0 +1,347 @@
+package com.usac.ipc1.p2;
+
+import java.awt.GridBagConstraints;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.JSlider;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author erick
+ */
+@SuppressWarnings("serial")
+public class PlotterFrame extends JFrame {
+
+    private JLabel lblPath, lblImg;
+    private JButton btnFindPath, btnGenGraph, btnSortGraph;
+    private JTextField txtTitleGraph;
+    private ButtonGroup btnGAlgorithm, btnGType;
+    private JRadioButton rbtnAsce, rbtnDesc, rbtnBubble, rbtnShell, rbtnQuick;
+    private JSlider slidVelocity;
+
+    public PlotterFrame() {
+        init();
+    }
+
+    private void init() {
+        GridBagConstraints gridBag;
+
+        btnGAlgorithm = new ButtonGroup();
+        btnGType = new ButtonGroup();
+
+        setMinimumSize(new Dimension(800, 600));
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("[IPC1] - Práctica 2");
+
+        getContentPane().setLayout(new GridBagLayout());
+        // -----------------------------------------------------
+        // Panel que contendrá todo lo demás
+        // -----------------------------------------------------
+        JPanel panel01 = new JPanel(new GridBagLayout());
+        //panel01.setBackground(Color.LIGHT_GRAY);
+        panel01.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.weightx = 1.0;
+        gridBag.weighty = 1.0;
+        getContentPane().add(panel01, gridBag);
+        // -----------------------------------------------------
+        // Panel superior
+        // -----------------------------------------------------
+        JPanel upperPanel = new JPanel(new GridBagLayout());
+        upperPanel.setBorder(BorderFactory.createEtchedBorder());
+        //upperPanel.setBackground(Color.WHITE);
+        upperPanel.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.weightx = 1.0;
+        gridBag.insets = new Insets(5, 5, 5, 5);
+        panel01.add(upperPanel, gridBag);
+        // -----------------------------------------------------
+        // Panel inferior
+        // -----------------------------------------------------
+        JPanel bottomPanel = new JPanel(new GridBagLayout());
+        //bottomPanel.setBackground(Color.WHITE);
+        bottomPanel.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 1;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.weightx = 1.0;
+        gridBag.weighty = 1.0;
+        gridBag.insets = new Insets(5, 5, 5, 5);
+        panel01.add(bottomPanel, gridBag);
+        // -----------------------------------------------------
+        // Diseño panel superior
+        // -----------------------------------------------------
+        // ETIQUETA PATH
+        lblPath = new JLabel(" ");
+        lblPath.setFont(new Font("MS Reference Sans Serif", Font.ITALIC, 12));
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.weightx = 1.0;
+        gridBag.insets = new Insets(5, 5, 10, 0);
+        upperPanel.add(lblPath, gridBag);
+        // BOTON PATH
+        btnFindPath = new JButton("Examinar...");
+        btnFindPath.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+        btnFindPath.setHorizontalAlignment(SwingConstants.CENTER);
+        btnFindPath.addActionListener((ActionEvent e) -> {
+            //TODO: acción para el boton
+        });
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 1;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(5, 0, 10, 5);
+        upperPanel.add(btnFindPath, gridBag);
+        // CAMPO DE TEXTO, TITULO
+        txtTitleGraph = new JTextField();
+        txtTitleGraph.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 1;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 5, 0, 0);
+        upperPanel.add(txtTitleGraph, gridBag);
+        // ETIQUETA TITULO
+        JLabel lblTitle = new JLabel("Título de la gráfica");
+        lblTitle.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 10));
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitle.setBackground(Color.LIGHT_GRAY);
+        lblTitle.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 2;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 5, 5, 0);
+        upperPanel.add(lblTitle, gridBag);
+        // BOTON GENERAR GRAFICA
+        btnGenGraph = new JButton("Generar gráfica");
+        btnGenGraph.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+        btnGenGraph.setHorizontalAlignment(SwingConstants.CENTER);
+        btnGenGraph.addActionListener((ActionEvent e) -> {
+            //TODO: acción para el boton
+        });
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 1;
+        gridBag.gridy = 1;
+        gridBag.gridheight = 2;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.insets = new Insets(0, 0, 5, 5);
+        upperPanel.add(btnGenGraph, gridBag);
+        // -----------------------------------------------------
+        // Diseño panel inferior
+        // -----------------------------------------------------
+        JPanel leftPanel = new JPanel(new GridBagLayout());
+        leftPanel.setBorder(BorderFactory.createEtchedBorder());
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.weightx = 1.0;
+        gridBag.weighty = 1.0;
+        gridBag.insets = new Insets(0, 0, 0, 5);
+        bottomPanel.add(leftPanel, gridBag);
+
+        lblImg = new JLabel(" ");
+        lblImg.setBackground(Color.WHITE);
+        lblImg.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.weightx = 1.0;
+        gridBag.weighty = 1.0;
+        leftPanel.add(lblImg, gridBag);
+        // -----------------------------------------------------
+        JPanel rightPanel = new JPanel(new GridBagLayout());
+        rightPanel.setBorder(BorderFactory.createEtchedBorder());
+        //rightPanel.setBackground(Color.WHITE);
+        rightPanel.setOpaque(true);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 1;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.BOTH;
+        gridBag.insets = new Insets(0, 5, 0, 0);
+        bottomPanel.add(rightPanel, gridBag);
+        // ETIQUETA ALGORITMO
+        JLabel lblAlgorithm = new JLabel("Algoritmo");
+        lblAlgorithm.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(5, 5, 0, 0);
+        rightPanel.add(lblAlgorithm, gridBag);
+        // OPCION BUBBLE
+        rbtnBubble = new JRadioButton("Bubblesort");
+        rbtnBubble.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+        btnGAlgorithm.add(rbtnBubble);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 1;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 5, 0, 0);
+        rightPanel.add(rbtnBubble, gridBag);
+        // OPCION QUICK
+        rbtnQuick = new JRadioButton("Quicksort");
+        rbtnQuick.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+        btnGAlgorithm.add(rbtnQuick);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 2;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 5, 0, 0);
+        rightPanel.add(rbtnQuick, gridBag);
+        // OPCION SHELL
+        rbtnShell = new JRadioButton("Shellsort");
+        rbtnShell.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+        btnGAlgorithm.add(rbtnShell);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 3;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 5, 0, 0);
+        rightPanel.add(rbtnShell, gridBag);
+        // SEPARADOR
+        JSeparator sep0 = new JSeparator();
+        sep0.setOrientation(SwingConstants.VERTICAL);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 1;
+        gridBag.gridy = 0;
+        gridBag.gridheight = 4;
+        gridBag.fill = GridBagConstraints.BOTH;
+        rightPanel.add(sep0, gridBag);
+        // ETIQUETA SENTIDO DE ORDENACION
+        JLabel lblSenseSort = new JLabel("Tipo");
+        lblSenseSort.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 2;
+        gridBag.gridy = 0;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(5, 0, 0, 5);
+        rightPanel.add(lblSenseSort, gridBag);
+        // OPCION ASC
+        rbtnAsce = new JRadioButton("Ascendente");
+        rbtnAsce.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+        btnGType.add(rbtnAsce);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 2;
+        gridBag.gridy = 1;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 0, 0, 5);
+        rightPanel.add(rbtnAsce, gridBag);
+        // OPCION DESC
+        rbtnDesc = new JRadioButton("Descendente");
+        rbtnDesc.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+        btnGType.add(rbtnDesc);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 2;
+        gridBag.gridy = 2;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(0, 0, 0, 5);
+        rightPanel.add(rbtnDesc, gridBag);
+        // SEPARADOR
+        JSeparator sep1 = new JSeparator();
+        sep1.setOrientation(SwingConstants.HORIZONTAL);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 4;
+        gridBag.gridwidth = 3;
+        gridBag.fill = GridBagConstraints.BOTH;
+        rightPanel.add(sep1, gridBag);
+        // ETIQUETA VELOCIDAD
+        JLabel lblVelocity = new JLabel("Velocidad");
+        lblVelocity.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 5;
+        gridBag.gridwidth = 3;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.insets = new Insets(5, 5, 0, 5);
+        rightPanel.add(lblVelocity, gridBag);
+        // SLIDER VELOCIDAD
+        slidVelocity = new JSlider(0,2,1);
+        slidVelocity.setPaintLabels(true);
+        slidVelocity.setPaintTicks(false);
+        slidVelocity.setSnapToTicks(true);
+        var lblSlide = slidVelocity.createStandardLabels(1, 0);
+        lblSlide.put(0, new JLabel(new ImageIcon("slow.png")));
+        lblSlide.put(1, new JLabel(new ImageIcon("medium.png")));
+        lblSlide.put(2, new JLabel(new ImageIcon("fast.png")));
+        slidVelocity.setLabelTable(lblSlide);
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 6;
+        gridBag.gridwidth = 3;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.weightx = 1.0;
+        gridBag.insets = new Insets(0, 5, 0, 5);
+        rightPanel.add(slidVelocity, gridBag);
+        // BOTON ORDENAR
+        btnSortGraph = new JButton("Ordenar gráfica");
+        btnSortGraph.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
+        btnSortGraph.setHorizontalAlignment(SwingConstants.CENTER);
+        btnSortGraph.addActionListener((ActionEvent e) -> {
+            //TODO: acción para el boton
+        });
+
+        gridBag = new GridBagConstraints();
+        gridBag.gridx = 0;
+        gridBag.gridy = 7;
+        gridBag.gridwidth = 3;
+        gridBag.fill = GridBagConstraints.HORIZONTAL;
+        gridBag.weightx = 1.0;
+        gridBag.insets = new Insets(5, 5, 5, 5);
+        rightPanel.add(btnSortGraph, gridBag);
+
+        pack();
+    }
+
+}
